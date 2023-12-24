064e4d3430ae47ebb0c9aab56f5f4774242ff576
maksim-bolotin <150610016+maksim-bolotin@users.noreply.github.com>
2023-12-24 20:56:23 +0300

    Реализован класс Item: инициализация, методы отвечающие за подсчёт общей стоимости товара и за подсчёт стоимости конкретного товара с учётом скидки, реализован метод all, для отображения списка экземпляров класса. (#1)
    Написаны тесты для класса Item.

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,28 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+from src.item import Item
+
+
+@pytest.fixture()
+def get_item():
+    Item.pay_rate = 0.7
+    return [Item("Смартфон", 10000, 20), Item("Бритва", "", "1")]
+
+
+def test_calculate_total_price(get_item):
+    assert get_item[0].calculate_total_price() == 200000
+    with pytest.raises(Exception):
+        get_item[1].calculate_total_price()
+
+
+def test_apply_discount(get_item):
+    get_item[0].apply_discount()
+    assert get_item[0].price == 7000
+
+
+def test__init__(get_item):
+    assert get_item[0].name == "Смартфон"
+    assert get_item[1].name == "Бритва"
+    assert get_item[0].price == 10000
+    assert get_item[0].quantity == 20
+    assert len(get_item[0].all) == 6
